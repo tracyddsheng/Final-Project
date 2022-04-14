e13cb37358f8db6e813c7570bcf417c13022b69c
tracyddsheng <89175441+tracyddsheng@users.noreply.github.com>
2022-04-14 12:17:30 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
+int height = 4, width = 4, minecount = 2, choicex, choicey;
 
+int main() {
+  
+   char griddy[width][height];
+  /*while (minecount>0){
+    if(R)
+  }
+
+  /*std::cout << "Welcome to MineC++!";
+  std::cout << "Enter the width of the board plz\n";
+  std::cin >> width;
+  std::cout << "Enter the height of the board\n";
+  std::cin >> height;*/
+
+  cout << "  __  __ _             ____\n |  \\/  (_)_ __   ___ / ___| _     _\n | |\\/| | | '_ \\ / _ \\ |   _| |_ _| |_\n | |  | | | | | |  __/ |__|_   _|_   _|\n |_|  |_|_|_| |_|\\___|\\____||_|   |_|\n";
+  cout << "By Eden Harvey, Thushar Reppale, and Tracy Sheng\n";
+  cout << "clear the board without detonating any mines\n rows and columns 0-8, F to flag, C to clear\n";
+  cout << "Press Enter to Start";
+  //cin >>
+  /*cin >> level;
+  if (level == 1) {
+    height = 9;
+    width = 9;
+    mines = 10;
+  } else if (level == 2) {
+    height = 16;
+    width = 16;
+    mines = 40;
+  } else if (level == 3){
+    height = 16;
+    width = 30;
+    mines = 99;
+  }*/
+
+
+}
